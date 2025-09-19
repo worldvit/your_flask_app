6b62b48c358633e7726c7e94b6e0fc52f12faf8c
worldvit <worldvit@github.com>
2025-09-19 09:50:11 +0900

    Modify wagi.py

--- wsgi.py
+++ wsgi.py
@@ -8,10 +8,6 @@ project_home = '/var/www/html/your_flask_app'
 if project_home not in sys.path:
     sys.path.insert(0, project_home) # 리스트의 맨 앞에 추가
 
-# Note: activate_this.py를 사용하는 대신, mod_wsgi의 WSGIDaemonProcess 설정에서
-# python-home 및 python-path를 통해 가상 환경을 지정합니다.
-# 따라서 여기서 별도로 가상 환경을 활성화할 필요가 없습니다.
-
 # Flask 애플리케이션 인스턴스 임포트
 # 'application'은 mod_wsgi가 기대하는 기본 이름입니다.
 from app import app as application
